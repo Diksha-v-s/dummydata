4ce2898071900b3752f8d64ebe00c903cbe18d4b
Diksha Sharma <91596289+Diksha-v-s@users.noreply.github.com>
2021-12-31 08:08:11 +0530

    Create StoreCustomers.sql

---
+++ StoreUsers/StoreCustomers.sql
@@ -0,0 +1,44 @@
+DELIMITER $$
+
+CREATE DEFINER=`thiorjhwewf`@`%` PROCEDURE `StoreCustomers`()
+BEGIN
+DECLARE a INT;
+SET a=1;
+        Label1: WHILE a<=50 DO
+          INSERT INTO `Users` (`UserName`, `UserFirstName`, `UserLastName`, `UserPhone`, `UserEmail`, `Address1`, `Address2`, `City`, `State`, `Zip`, `UserPassword`, `UserPasswordSalt`, `Comments`, `UserCreateTS`, `UserLastUpdatedDateTime`, `UserBlocked`, `PermanentUserBlocked`, `EmailVerified`, `PhoneVerified`, `ForcePasswordChange`) VALUES 
+          (
+          LCASE(LPAD(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(TO_BASE64(UNHEX(HEX(MD5(RAND())))), "/", ""), "+", ""), "=", ""), "o", ""), "a", ""), "g", ""), "h", ""), "p", ""), "o", ""), 10), 10, 2)), 
+          LCASE(concat(char(round(rand()*25)+65),char(round(rand()*25)+65),char(round(rand()*25)+65),char(round(rand()*25)+65),  char(round(rand()*25)+65),char(round(rand()*25)+65),char(round(rand()*25)+65))), 
+          LCASE(concat(char(round(rand()*25)+65),char(round(rand()*25)+65),char(round(rand()*25)+65),char(round(rand()*25)+65),  char(round(rand()*25)+65),char(round(rand()*25)+65),char(round(rand()*25)+65))), 
+          LPAD(FLOOR(RAND() * 70000000000), 10, '0'), 
+          CONCAT("nana", ROUND(RAND() * 100),'@yopmail.com'), 
+          LCASE(LPAD(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(TO_BASE64(UNHEX(HEX(MD5(RAND())))), "/", ""), "+", ""), "=", ""), "o", ""), "a", ""), "g", ""), "h", ""), "p", ""), "o", ""), 10), 10, 2)), 
+          LCASE(LPAD(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(TO_BASE64(UNHEX(HEX(MD5(RAND())))), "/", ""), "+", ""), "=", ""), "o", ""), "a", ""), "g", ""), "h", ""), "p", ""), "o", ""), 10), 10, 2)),
+          LCASE(LPAD(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(TO_BASE64(UNHEX(HEX(MD5(RAND())))), "/", ""), "+", ""), "=", ""), "o", ""), "a", ""), "g", ""), "h", ""), "p", ""), "o", ""), 10), 10, 2)),
+          LCASE(LPAD(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(TO_BASE64(UNHEX(HEX(MD5(RAND())))), "/", ""), "+", ""), "=", ""), "o", ""), "a", ""), "g", ""), "h", ""), "p", ""), "o", ""), 10), 10, 2)), 
+          ROUND(10.0+40.0*RAND()*1000), 
+          CHAR( FLOOR(65 + (RAND() * 10))),FLOOR(@number * rand()) + 1,
+          ' ',
+          current_timestamp(),
+          current_timestamp(), '0', '0', '0', '0', '0');
+
+        INSERT INTO `etogeoie23434`.`UserRoles` ( `UserKey`, `RoleKey`, `UserKey_LastUpdatedBy`, `UserKey_CreatedBy`, `UserRoleCreateTS`, `UserRoleLastUpdatedDateTime`) VALUES
+          ( (Select UserKey from Users order by rand() LIMIT 1), 
+          '6',
+          (select a.UserKey from UserRoles a inner join Roles b on a.RoleKey=b.RoleKey && b.RoleName="Admin" order by rand() LIMIT 1),
+          (select a.UserKey from UserRoles a inner join Roles b on a.RoleKey=b.RoleKey && b.RoleName="Admin" order by rand() LIMIT 1), 
+          current_timestamp(), 
+           current_timestamp());
+
+        INSERT INTO `etogeoie23434`.`StoreUsers` (`StoreKey`,`UserKey`, `UserKey_LastUpdatedBy`, `UserKey_CreatedBy`, `StoreUsersCreateTS`, `StoreUsersLastUpdatedDateTime`) VALUES 
+        ((select StoreKey from Stores order by rand() LIMIT 1) ,
+        (select UserKey from Users order by rand() LIMIT 1),
+        (select a.UserKey from UserRoles a inner join Roles b on a.RoleKey=b.RoleKey && b.RoleName="Admin" order by rand() LIMIT 1),
+        (select a.UserKey from UserRoles a inner join Roles b on a.RoleKey=b.RoleKey && b.RoleName="Admin" order by rand() LIMIT 1),
+        current_timestamp(), 
+        current_timestamp());
+
+
+SET a=a+1;
+        END WHILE label1;
+end
